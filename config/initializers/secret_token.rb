d1144d0029087bfa5bb143f29d44b06f6ef84e65
slicebo123 <slicebo123@gmail.com>
2014-03-31 21:17:17 -0700

    Blank Rails New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NprBumps::Application.config.secret_key_base = '62040818f7f6608ef72d05577c2429783477abecd738b922133d3fb721fec3a40f55ec900992589eb20c8473237efcf0dbfb0262cfcd3bb931a26b71a87ae3ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NprBumps::Application.config.session_store :cookie_store, key: '_npr_bumps_session'
