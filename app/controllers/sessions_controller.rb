db3184c510185c3eae5dd59bd3a48ac9f7b7fe83
slicebo123 <slicebo123@gmail.com>
2014-07-31 17:34:49 -0700

    Remove errant binding.pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
   def create
-    binding.pry
     @person = Person.find_or_create_from_oauth(auth_hash)
     #find or create using Spotify data
     # create access token for User to support FE API calls
