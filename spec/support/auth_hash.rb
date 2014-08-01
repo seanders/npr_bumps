33e47733656aff30412bd7ee6a2d0a2549bf80b6
slicebo123 <slicebo123@gmail.com>
2014-07-31 17:26:50 -0700

    Fake Spotify auth data

---
+++ spec/support/auth_hash.rb
@@ -0,0 +1,34 @@
+def oauth_hash
+  {"provider"=>"spotify",
+   "uid"=>"1",
+   "info"=>
+   {"name"=>"Billy Mays",
+     "nickname"=>"billy_mays",
+     "email"=>"fake@gmail.com",
+     "urls"=>{"spotify"=>"https://open.spotify.com/user/1"},
+     "image"=>
+     "https://fake_url.jpg"},
+     "credentials"=>
+     {"token"=>
+      "fake_token",
+      "refresh_token"=>
+      "fake_refresh_token",
+      "expires_at"=>1406849118,
+      "expires"=>true},
+      "extra"=>
+      {"raw_info"=>
+        {"country"=>"US",
+         "display_name"=>"Billy Mays",
+         "email"=>"fake@gmail.com",
+         "external_urls"=>{"spotify"=>"https://open.spotify.com/user/1"},
+         "href"=>"https://api.spotify.com/v1/users/1",
+         "id"=>"1",
+         "images"=>
+         [{"height"=>nil,
+          "url"=>
+          "http://fake_url.jpg",
+          "width"=>nil}],
+          "product"=>"premium",
+          "type"=>"user",
+          "uri"=>"spotify:user:1"}}}
+end
