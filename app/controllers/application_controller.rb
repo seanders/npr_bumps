3d2a17c5efbecb6506e34246313dc91af2606ace
slicebo123 <slicebo123@gmail.com>
2014-12-04 23:09:32 -0800

    Add base layout. Add javascript tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout 'base'
 
   def require_person
     @person ||= Person.find(params[:id])
