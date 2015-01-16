5484479b25f405c9e106b687fa5869ccf3710f0a
Sean Miller <slicebo123@gmail.com>
2015-01-16 00:26:42 -0800

    Move subscription form logic into form component

--- app/assets/javascripts/components/PlaylistContainer.js.jsx
+++ app/assets/javascripts/components/PlaylistContainer.js.jsx
@@ -9,46 +9,15 @@ var PlaylistContainer = React.createClass({
   getInitialState: function () {
     return {
       playlistId: null,
-      playlistItem: {},
-      dirtySubscriptionsForm: false,
-      subscriptions: [],
-      masterSubscriptions: []
+      playlistItem: {}
     };
   },
 
   containerGetSubscriptionsForItem: function (playlistItem) {
     var playlistId = playlistItem.id;
-        self = this,
-        api = new prBumpsApi();
-
-    api.getSubscriptions(playlistId).then(function (response) {
-      self.setState({
-        masterSubscriptions: response,
-        subscriptions: _.cloneDeep(response),
-        playlistId: playlistId,
-        playlistItem: playlistItem
-      })
-    });
-  },
-
-  isSubscriptionFormDirty: function (oldList, newList) {
-    return !_.isEqual(oldList, newList);
-  },
-
-  handleClickOnSubscriptionItem: function (playlistId, subscribed) {
-    // update subscriptions.subscribed value for correct playlist
-    var newSubscriptions = this.state.subscriptions;
-    var masterSubscriptions = this.state.masterSubscriptions;
-    var playlist = _.find(newSubscriptions, {id: playlistId});
-
-    // set value on playlistItem
-    playlist.subscribed = subscribed;
-    // check if value we are setting is different from original form
-    var dirtyForm = this.isSubscriptionFormDirty(masterSubscriptions, newSubscriptions);
-    // update state
     this.setState({
-      subscriptions: newSubscriptions,
-      dirtySubscriptionsForm: dirtyForm
+      playlistId: playlistId,
+      playlistItem: playlistItem
     });
   },
 
@@ -58,7 +27,7 @@ var PlaylistContainer = React.createClass({
     return (
       <div>
         <PlaylistIndex containerGetSubscriptionsForItem={this.containerGetSubscriptionsForItem}></PlaylistIndex>
-        <SubscriptionList playlistName={playlistItem.name} clickSubscriptionHandler={this.handleClickOnSubscriptionItem} items={subscriptionItems} playlistId={playlistItem.id}></SubscriptionList>
+        <SubscriptionList playlistItem={playlistItem}></SubscriptionList>
       </div>
     )
   }
--- app/assets/javascripts/components/SubscriptionList.js.jsx
+++ app/assets/javascripts/components/SubscriptionList.js.jsx
@@ -1,16 +1,59 @@
 var React = require('react'),
     SubscriptionItem = require('./SubscriptionItem.js.jsx'),
+    _ = require('lodash'),
     mui = require('material-ui'),
     RaisedButton = mui.RaisedButton;
 
 var SubscriptionList = React.createClass({
+  getInitialState: function () {
+    return {
+      dirtySubscriptionsForm: false,
+      subscriptions: [],
+      masterSubscriptions: []
+    };
+  },
+
+  componentWillReceiveProps: function (nextProps) {
+    var self = this;
+    this.getSubscriptionsForPlaylist(nextProps.playlistItem).then(function (response) {
+      self.setState({
+        masterSubscriptions: response,
+        subscriptions: _.cloneDeep(response)
+      })
+    });
+  },
+
+  getSubscriptionsForPlaylist: function (playlistItem) {
+    var playlistId = playlistItem.id,
+        api = new prBumpsApi();
+
+    return api.getSubscriptions(playlistId);
+  },
+
   onChangeHandler: function (playlistId, subscribed) {
-    this.props.clickSubscriptionHandler(playlistId, subscribed);
+    // update subscriptions.subscribed value for correct playlist
+    var newSubscriptions = this.state.subscriptions;
+    var masterSubscriptions = this.state.masterSubscriptions;
+    var playlist = _.find(newSubscriptions, {id: playlistId});
+
+    // set value on playlistItem
+    playlist.subscribed = subscribed;
+    // check if value we are setting is different from original form
+    var isFormDirty = this.isSubscriptionFormDirty(masterSubscriptions, newSubscriptions);
+    // update state
+    this.setState({
+      subscriptions: newSubscriptions,
+      dirtySubscriptionsForm: isFormDirty
+    });
+  },
+
+  isSubscriptionFormDirty: function (oldList, newList) {
+    return !_.isEqual(oldList, newList);
   },
 
   render: function() {
     var self = this;
-    var subscriptionItems = this.props.items.map(function (subscription) {
+    var subscriptionItems = this.state.subscriptions.map(function (subscription) {
       return (
         <SubscriptionItem onChangeHandler={self.onChangeHandler} name={subscription.name} programId={subscription.id} key={subscription.id} subscribed={subscription.subscribed} />
       )
@@ -18,9 +61,9 @@ var SubscriptionList = React.createClass({
 
     return (
       <div className="subscription-index">
-        <h3>{this.props.playlistName}</h3>
+        <h3>{this.props.playlistItem.name}</h3>
         <form>
-          <input type="hidden" id="playlistId" name="playlistId" value={this.props.playlistId}/>
+          <input type="hidden" id="playlistId" name="playlistId" value={this.props.playlistItem.id}/>
           {subscriptionItems}
         </form>
         <div className="primary-action-button">
